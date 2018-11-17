ab1c0e76f84b295d5af4af7e693017cafcbdb5cf
Raymond Kwan <rayk199492@gmail.com>
2018-11-17 10:56:22 -0500

    Initial commit, basic server

---
+++ server.py
@@ -0,0 +1,35 @@
+import time
+import json
+from uuid import uuid4
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+HOST_NAME = 'localhost'
+PORT_NUMBER = 9000
+
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+      if self.path == '/userid':
+        data = {'userId': str(uuid4())}
+      else:
+        data = {'nothingHere': 123}
+      data = json.dumps(data)
+      self.send_response(200)
+      self.send_header('Content-type', 'application/json')
+      self.end_headers()
+      self.wfile.write(bytes(data, 'UTF-8'))
+
+    def respond(self, data):
+      data = json.dumps(data)
+
+
+if __name__ == '__main__':
+    server_class = HTTPServer
+    httpd = server_class((HOST_NAME, PORT_NUMBER), MyHandler)
+    print(time.asctime(), 'Server Starts - %s:%s' % (HOST_NAME, PORT_NUMBER))
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
