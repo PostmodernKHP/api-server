66e6151b0a99520d32cb78c7f9e22cb0ff64fe54
Raymond Kwan <rayk199492@gmail.com>
2018-11-17 13:36:02 -0500

    Added options request allow cors

--- server.py
+++ server.py
@@ -12,6 +12,13 @@ PENDING_USERS = {
   }
 
 class MyHandler(BaseHTTPRequestHandler):
+
+   def do_OPTIONS(self):
+        self.send_response(200, "ok")
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header("Access-Control-Allow-Headers", "X-Requested-With")
+
     def do_GET(self):
       # Handle for different GET endpoints
       if self.path == '/userid':
